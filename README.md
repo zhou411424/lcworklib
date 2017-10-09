f6640c63792cffb1d45dd53719598996110d2f93
zhoulc <zhouliancheng@koolearn.com>
2017-10-09 17:31:08 +0800

    add widget

---
+++ README.md
@@ -0,0 +1,2 @@
+上传命令：
+gradlew clean build bintrayUpload -PbintrayUser=zhoulc668 -PbintrayKey=******************** -PdryRun=false
---
+++ lcworklibrary/src/main/java/com/lcwork/library/widget/CustomScrollView.java
@@ -0,0 +1,51 @@
+package com.lcwork.library.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.ViewConfiguration;
+import android.widget.ScrollView;
+
+/**
+ * Created by zhouliancheng on 2016/12/21.
+ * 防止滑动事件冲突，屏蔽滑动事件
+ */
+public class CustomScrollView extends ScrollView {
+    private int downX;
+    private int downY;
+    private int mTouchSlop;
+
+    public CustomScrollView(Context context) {
+        this(context, null);
+    }
+
+    public CustomScrollView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public CustomScrollView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context);
+    }
+
+    private void init(Context context) {
+        mTouchSlop = ViewConfiguration.get(context).getScaledTouchSlop();
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent e) {
+        int action = e.getAction();
+        switch (action) {
+            case MotionEvent.ACTION_DOWN:
+                downX = (int) e.getRawX();
+                downY = (int) e.getRawY();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                int moveY = (int) e.getRawY();
+                if (Math.abs(moveY - downY) > mTouchSlop) {
+                    return true;
+                }
+        }
+        return super.onInterceptTouchEvent(e);
+    }
+}
